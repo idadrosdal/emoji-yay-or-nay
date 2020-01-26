b49ab3c5f3c022b9b3dc224902f7efaaef09a7d0
Ida Drøsdal <idadrosdal@gmail.com>
2020-01-26 18:28:25 +0100

    Added new styling and created tables for yay/nay lists

--- emoji-yay-or-nay/scripts/main.js
+++ emoji-yay-or-nay/scripts/main.js
@@ -1,8 +1,11 @@
+const defaultEmoji = String.fromCodePoint(0x1F636);
+const altEmoji = String.fromCodePoint(0x1F92A);
 let emojiElem = document.getElementById("header-emoji");
+emojiElem.innerHTML = defaultEmoji;
 emojiElem.onclick = function() {
-	if (emojiElem.innerHTML.normalize() == String.fromCodePoint(0x1F636).normalize()) {
-		emojiElem.innerHTML = "&#x1F92A";
+	if (emojiElem.innerHTML.normalize() == defaultEmoji.normalize()) {
+		emojiElem.innerHTML = altEmoji;
 	} else {
-		emojiElem.innerHTML = "&#x1F636;";
+		emojiElem.innerHTML = defaultEmoji;
 	}
 }
